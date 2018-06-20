ecb98f71d48785f5a28dba2dcad35027e36e36db
Taylor Johnson <tjohnson@metrocommercial.com>
2018-06-20 09:47:48 -0400

    make the geocoder great again

--- app.js
+++ app.js
@@ -60,12 +60,23 @@ require([
 
     const submitButton = $('#submit-button');
     submitButton.on('click', function(e) {
-        
+        let geocodeSpeed = 0;
+        if ($('#showOnMapBox').is(":checked")) {
+            geocodeSpeed = 2000;
+        }
+        //console.log(geocodeSpeed);
 
         $('#latlon-out').val("");
         let addressList = $('#address-in').val();
 
-        const length = addressList.split('\n').length - 1;
+        //console.log(view.graphics.items);
+        if (view.graphics) view.graphics.items = [];
+
+        let length = addressList.split('\n').length;
+        if (addressList.split('\n')[addressList.split('\n').length - 1].length == 0) {
+            length--;
+        }
+        //console.log(length);
         let currentNum = 1;
         addressList.split('\n').forEach(function(item, index) {
             if (item.length > 0) {              
@@ -75,7 +86,7 @@ require([
                     setTimeout( () => { 
                         $('#complete').text(`${currentNum} / ${length} Complete`);
                         currentNum += 1;
-                        console.log(currentNum);
+                        //console.log(currentNum);
 
                         let output = '';
 
@@ -89,29 +100,31 @@ require([
                         outText.val(outText.val() + output + '\n');            
 
                         //let point = {type: 'point', latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng};
-                        const point = {
-                            type: "point",
-                            longitude: data.results[0].geometry.location.lng,
-                            latitude: data.results[0].geometry.location.lat
-                        };
-                    
-                        const markerSymbol = {
-                            type: "simple-marker",
-                            outline: {
-                                style: "none"
-                            },
-                            size: 12,
-                            color: [255, 0, 0, 1]
-                        };
-                    
-                        const pointGraphic = new Graphic({
-                            geometry: point,
-                            symbol: markerSymbol
-                        });
-                    
-                        view.graphics.add(pointGraphic)
-                        view.goTo({target: pointGraphic, zoom: 15});
-                        }, index * 1500);
+                        if ($('#showOnMapBox').is(":checked")) {
+                            const point = {
+                                type: "point",
+                                longitude: data.results[0].geometry.location.lng,
+                                latitude: data.results[0].geometry.location.lat
+                            };
+                        
+                            const markerSymbol = {
+                                type: "simple-marker",
+                                outline: {
+                                    style: "none"
+                                },
+                                size: 12,
+                                color: [255, 0, 0, 1]
+                            };
+                        
+                            const pointGraphic = new Graphic({
+                                geometry: point,
+                                symbol: markerSymbol
+                            });
+                        
+                            view.graphics.add(pointGraphic)
+                            view.goTo({target: pointGraphic, zoom: 15});
+                        }
+                        }, index * geocodeSpeed);
                     }
                 });
             }
